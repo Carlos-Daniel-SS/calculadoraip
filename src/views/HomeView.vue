b25a454ad60638057daf17b4b330b40b82f8a9e0
Carlos Daniel <carlossousa@rede.ulbra.br>
2023-05-07 21:24:55 -0300

    Criação do Template temporário

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,104 @@
 <template>
-  <HelloWorld />
+  <v-container class="spacing-playground pa-6">
+    <v-row>
+      <v-col> </v-col>
+      <v-col>
+        <h1 class="text-center">{{ endereco_ip }}</h1>
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+    <br />
+
+    <v-row no-gutters>
+      <v-col>
+        <form>
+          <v-text-field
+            v-model="endereco_ip"
+            class="pe-2"
+            placeholder="Ex.: 133.8.0.0"
+            label="Digite o endereço de rede:"
+          >
+          </v-text-field>
+        </form>
+      </v-col>
+      <v-col>
+        <form>
+          <v-text-field
+            class="pe-2"
+            placeholder="Ex.: 133.8.0.1"
+            label="Digite o endereço de host:"
+          >
+          </v-text-field>
+        </form>
+      </v-col>
+      <v-col>
+        <form>
+          <v-text-field
+            v-model="mascara"
+            class="pe-2"
+            placeholder="Ex.: 255.255.255.0"
+            label="Máscara:"
+          >
+          </v-text-field>
+        </form>
+      </v-col>
+    </v-row>
+
+    <v-row no-gutters>
+      <v-col>
+        <form>
+          <v-text-field
+            class="pe-2"
+            placeholder="Ex.: 133.8.0.0/24"
+            label="Representação CIDR:"
+          >
+          </v-text-field>
+        </form>
+      </v-col>
+      <v-col>
+        <form>
+          <v-text-field
+            class="pe-2"
+            placeholder="Ex.: 133.8.0.255"
+            label="Digite o endereço de broadcast:"
+          >
+          </v-text-field>
+        </form>
+      </v-col>
+      <v-col>
+        <form>
+          <v-text-field
+            class="pe-2"
+            placeholder="Ex.: 16"
+            label="N° de sub-redes a serem ciradas:"
+          >
+          </v-text-field>
+        </form>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col> </v-col>
+      <v-col align="center" jutify="center">
+        <v-btn class="bg-green" width="100%"
+          >Calcular</v-btn
+        >
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+export default {
+  name: "HomeView",
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      endereco_ip: "",
+      mascara: "",
+    };
   },
-});
+  
+};
 </script>
